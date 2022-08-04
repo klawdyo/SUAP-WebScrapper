2ccfbd939a8c3a253bb7e7f8954172439c0daebd
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-04 09:45:00 -0300

    refatoration: Remoção de código velho

--- src/modules/suap/user/user.repository.ts
+++ src/modules/suap/user/user.repository.ts
@@ -1,5 +1,5 @@
 import User from "../../../models/user";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient().user;
 
 export default {
