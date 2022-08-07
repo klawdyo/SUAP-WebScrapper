329997acd97242c9f00179e4517f914c578f6fde
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-07 09:00:01 -0300

    fix: Inclusão de rota padrão de erro

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,15 @@
 import suapAuthRouter from "modules/suap/auth/auth.router";
 import suapUserRouter from "modules/suap/user/user.router";
 
+//
+import errorRouter from "modules/suap/error/error.router";
+
 export default [
   //
   // itemRouter,
   suapAuthRouter,
   suapUserRouter,
+
+  // Rota de erro
+  errorRouter,
 ];
