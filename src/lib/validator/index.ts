fde382388171b684086d409cd015deeb8e344a18
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-06 21:24:03 -0300

    fix: Finalização do modelo de validação

--- src/lib/validator/index.ts
+++ src/lib/validator/index.ts
@@ -1,11 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+import messages from "./messages";
 import { ValidatorSchema, ValidatorData } from "./types";
 
 export default class Validator {
-  static schema?: ValidatorSchema;
+  static _schema?: ValidatorSchema;
   static data?: ValidatorData;
 
   static setSchema(schema: ValidatorSchema) {
-    this.schema = schema;
+    this._schema = schema;
     return this;
   }
 
@@ -16,7 +19,7 @@ export default class Validator {
 
   static async isValid() {
     try {
-      await this.schema?.validateAsync(this.data);
+      await this._schema?.validateAsync(this.data);
       return true;
     } catch (error) {
       return false;
@@ -25,11 +28,63 @@ export default class Validator {
 
   static async validate() {
     try {
-      return await this.schema?.validateAsync(this.data, {
+      return await this._schema?.validateAsync(this.data, {
         abortEarly: false,
       });
     } catch (error) {
       throw error;
+    }
+  }
+
+  // static _schema?: ValidatorSchema;
+
+  // constructor(schema: ValidatorSchema) {
+  //   this._schema = schema;
+  // }
+
+  static meupau(schema: Joi.PartialSchemaMap) {
+    Joi.preferences({
+      messages: {
+        "number.required": "preeencha",
+        number: "recea",
+        "number.base": "recebaa",
+      },
+    });
+
+    return Joi.object(schema);
+  }
+
+  static schema(schema: Joi.PartialSchemaMap) {
+    this._schema = Joi
+      // Inclui o objeto do esquema
+      .object(schema)
+      // Inclui as mensagens padronizadas
+      .messages(messages);
+
+    // Retorna o objeto para continuar o encadeamento
+    return this;
+  }
+
+  static async middleware(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      Validator.setData(request.body);
+
+      if (!this._schema)
+        throw { code: 500, message: "Esquema de validação não definido" };
+
+      Validator.setSchema(this._schema);
+
+      const body = await Validator.validate();
+      request.body = body;
+
+      return next();
+    } catch (error) {
+      console.log("erro no middleware", error);
+      return response.exception(error);
     }
   }
 }
---
+++ src/lib/validator/messages.ts
@@ -0,0 +1,9 @@
+//
+// As variáveis com {#var} representam o valores existentes
+// dentro do objeto context no retorno da validação
+//
+export default {
+  "any.required": "Campo obrigatório",
+  "number.base": "Não é um número",
+  "string.min": "Mínimo de {#limit} caracteres",
+};
