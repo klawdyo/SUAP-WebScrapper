f39228ff8641c3627a7752e8ea6601574ede8048
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-11 07:56:34 -0300

    fix: Inclusão de alterações para acesso na rede interna

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import app from "./app";
 const port = process.env.APP_PORT || 3000;
+const host = process.env.APP_HOST || "localhost";
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(+port, host, () => console.log(`Running on http://${host}:${port}`));
