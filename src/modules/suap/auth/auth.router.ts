1612031d7fa709e14ef919b9b1d2fc4e91ae6291
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-11 11:01:21 -0300

    feat: Inclusão de logout
    - Apaga o cookie salvo no banco de dados

--- src/modules/suap/auth/auth.router.ts
+++ src/modules/suap/auth/auth.router.ts
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/suap/auth/login", [loginValidator], AuthController.login);
 router.post("/suap/auth/logged", AuthController.logged);
 router.get("/suap/auth/profile", [authMiddleware], AuthController.profile);
+router.post("/suap/auth/logout", [authMiddleware], AuthController.logout);
 
 export default router;
