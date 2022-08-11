f9d62812eb8ec453057729d831b806f74a6af682
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-11 08:05:02 -0300

    feat: Inclusão de module home

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
-// import itemRouter from "./modules/item/item.router";
+import homeRouter from "modules/home/home.router";
+
+// SUAP
 import suapAuthRouter from "modules/suap/auth/auth.router";
 import suapUserRouter from "modules/suap/user/user.router";
 import suapPersonRouter from "modules/suap/person/person.router";
@@ -9,8 +11,9 @@ import suapTravelRouter from "modules/suap/travel/travel.router";
 import errorRouter from "modules/error/error.router";
 
 export default [
-  //
-  // itemRouter,
+  homeRouter,
+
+  // SUAP
   suapAuthRouter,
   suapUserRouter,
   suapPersonRouter,
