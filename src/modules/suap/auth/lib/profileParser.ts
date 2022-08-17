00db8cd0e5ef24f9e44c1cdd5888155ce15b6600
Cláudio Medeiros <klawdyo@gmail.com>
2022-08-17 09:51:29 -0300

    feat: Inclusão de nome usual e cpf no cadastro de usuário

--- src/modules/suap/auth/lib/profileParser.ts
+++ src/modules/suap/auth/lib/profileParser.ts
@@ -17,13 +17,50 @@ export default function profileParser(content: string): User {
     $content
   ).siblings("dd");
 
+  // Campus
+  const $campus = $('.list-item dt:contains("Setor SUAP")', $content).siblings(
+    "dd"
+  );
+
+  // Nome Curto
+  const $shortName = $('.list-item dt:contains("Nome Usual")').siblings("dd");
+
+  // CPF
+  const $cpf = $('.list-item dt:contains("CPF")').siblings("dd");
+
   // Image
   const $image = $(".photo-circle img", $content);
 
-  return new User({
+  const payload = {
     matricula: titleMatch?.groups?.matricula,
     name: titleMatch?.groups?.name,
+    cpf: $cpf.text(),
     email: $email.text(),
     image: SUAP.baseURL + $image.attr("src"),
-  });
+    campus: getCampus($campus.text()), // retorna a sigla do campus
+    shortName: $shortName.text(),
+  };
+
+  // console.log("Resultado do scrapping: ", payload);
+
+  return new User(payload);
+}
+
+/**
+ * Pega o campus do usuário e retorna
+ *
+ * getCampus('RE (campus: RE)')
+ * getCampus('DG/ZL (campus: DG/ZL)')
+ * getCampus('SEAC/IP (campus: DG/IP)')
+ *
+ * @param campus
+ * @returns
+ */
+function getCampus(campus: string): string | undefined {
+  const rgx = /([A-Z]+\/)?([A-Z]+)\)/i;
+  const match = rgx.exec(campus);
+
+  if (match) {
+    return match[2];
+  }
 }
