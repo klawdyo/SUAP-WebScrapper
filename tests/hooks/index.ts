6e23fb85ef8f6c2336c5a6da07b8de93eb0893d3
Cláudio Medeiros <klawdyo@gmail.com>
2022-10-03 10:28:04 -0300

    refatoration: Alteração do hook que autoriza as requests

--- tests/hooks/index.ts
+++ tests/hooks/index.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import login from "modules/suap/auth/lib/login";
 
 export const truncateTables = async () => {
   const prisma = new PrismaClient();
 
   await prisma.token.deleteMany();
   await prisma.user.deleteMany();
+};
+
+export const getAuthorization = async () => {
+  const data = await login(process.env.SUAP_USER!, process.env.SUAP_PASS!);
+  return { Authorization: `Bearer ${data.token!.access_token}` };
 };
