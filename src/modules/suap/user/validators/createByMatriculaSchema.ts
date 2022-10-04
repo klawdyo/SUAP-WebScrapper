7bfba4e12b87078a29f1b141a7aa734bd5050737
Cláudio Medeiros <klawdyo@gmail.com>
2022-10-04 10:51:22 -0300

    fix: Atualização das mensagens de erro

--- src/modules/suap/user/validators/createByMatriculaSchema.ts
+++ src/modules/suap/user/validators/createByMatriculaSchema.ts
@@ -1,7 +1,15 @@
 import Validator from "lib/validator";
 
 const createByMatriculaSchema = Validator.schema((v: any) => ({
-  matricula: v.string().required(),
+  matricula: v // <- objeto Joi
+    .string()
+    .required()
+    .regex(/^\d+$/)
+    .messages({
+      "string.base": "Matrícula precisa ser uma string",
+      "string.pattern.name": "Matrícula precisa ser numérico",
+      "string.empty": "Campo obrigatório",
+    }),
 }));
 
 export default createByMatriculaSchema;
